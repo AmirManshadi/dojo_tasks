9a6e939b75a877d396ca53c5c46ed728a2c5572c
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-06-18 14:45:43 +0330

    feat: add toast to logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { UnlockIcon } from "@chakra-ui/icons"
 import {
 	Avatar,
 	Button,
@@ -6,9 +7,24 @@ import {
 	Heading,
 	Spacer,
 	Text,
+	useToast,
 } from "@chakra-ui/react"
 
 export default function Navbar() {
+	const toast = useToast()
+
+	function handleClick() {
+		toast({
+			status: "success",
+			title: "Logged out successfully",
+			duration: 3000,
+			isClosable: true,
+			position: "bottom right",
+			variant: "subtle",
+			colorScheme: "blue",
+		})
+	}
+
 	return (
 		<Flex align="center" justifyContent="space-around" p={4}>
 			<Heading color="blue.700" as="h1">
@@ -18,7 +34,9 @@ export default function Navbar() {
 			<HStack spacing={4}>
 				<Avatar name="Amir" bg="blue.200" src="/public/img/mario.png"></Avatar>
 				<Text>AmirHossein</Text>
-				<Button colorScheme="blue">logout</Button>
+				<Button colorScheme="blue" onClick={handleClick}>
+					logout
+				</Button>
 			</HStack>
 		</Flex>
 	)
