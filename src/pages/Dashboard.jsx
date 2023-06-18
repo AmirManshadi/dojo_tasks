453afb3f9653c16e6595f79ee1e4747f6255d396
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-06-18 22:47:29 +0330

    feat: add dashboard content using loader function

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,57 @@
-import { Box, SimpleGrid } from "@chakra-ui/react"
+import { ChatIcon } from "@chakra-ui/icons"
+import {
+	Avatar,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	SimpleGrid,
+	Heading,
+	Button,
+	Text,
+} from "@chakra-ui/react"
+import { useLoaderData } from "react-router-dom"
+
+export async function DashboardLoader() {
+	const tasks = await fetch("http://localhost:3000/tasks")
+	return await tasks.json()
+}
 
 export default function Dashboard() {
+	const tasks = useLoaderData()
+
 	return (
-		<SimpleGrid minChildWidth="60" spacing={4} p={4}>
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
-			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+		<SimpleGrid minChildWidth="60" alignItems={"stretch"} spacing={4} p={4}>
+			{tasks.map(task => (
+				<Card
+					key={task.id}
+					shadow="base"
+					borderTop="4px"
+					borderTopColor="blue.400"
+					borderBottomLeftRadius="md"
+					borderBottomRightRadius="md"
+				>
+					<CardHeader
+						display="flex"
+						alignItems="center"
+						gap="2"
+						color="blackAlpha.800"
+					>
+						<Avatar name={task.author} src={task.img} />
+						<Heading as="h4" fontSize="md">
+							{task.title}
+						</Heading>
+					</CardHeader>
+					<CardBody color="blackAlpha.700" fontSize="sm" py="0">
+						{task.description}
+					</CardBody>
+					<CardFooter>
+						<Button leftIcon={<ChatIcon />} variant="ghost" fontSize="sm">
+							<Text>comment</Text>
+						</Button>
+					</CardFooter>
+				</Card>
+			))}
 		</SimpleGrid>
 	)
 }
