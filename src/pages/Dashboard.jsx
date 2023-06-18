db763530a933f8851041516c799abfc5d3a61ea6
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-06-18 15:12:06 +0330

    feat: add dashboard task grid view

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
+import { Box, SimpleGrid } from "@chakra-ui/react"
+
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+	return (
+		<SimpleGrid minChildWidth="60" spacing={4} p={4}>
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+			<Box shadow="base" borderTop="4px" borderTopColor="blue.400" borderBottomLeftRadius="md" borderBottomRightRadius="md" aspectRatio="4/3" />
+		</SimpleGrid>
+	)
 }
