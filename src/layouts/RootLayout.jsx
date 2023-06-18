7b66f9515ebb4377ea263dfa0bf1aead422d15a6
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-06-18 21:23:32 +0330

    feat: create main grid layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,35 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Sidebar from "../components/Sidebar"
+import { Grid, GridItem } from "@chakra-ui/react"
 
 export default function RootLayout() {
 	return (
-		<div>
-      <Sidebar />
-			<Navbar />
-			<Outlet />
-		</div>
+		<Grid templateColumns="repeat(6, 1fr)" templateRows="80px 80px auto">
+			<GridItem
+				colStart={[1]}
+				colEnd={[7, 3, 2]}
+				rowStart={[2, 1]}
+				rowEnd={[3, 4]}
+			>
+				<Sidebar />
+			</GridItem>
+			<GridItem 
+        colStart={[1, 3, 2]} 
+        colEnd={[7]} 
+        rowStart={[1]} 
+        rowEnd={2}
+      >
+				<Navbar />
+			</GridItem>
+			<GridItem
+				colStart={[1, 3, 2]}
+				colEnd={[7]}
+				rowStart={[3, 2]}
+				rowEnd={[4]}
+			>
+				<Outlet />
+			</GridItem>
+		</Grid>
 	)
 }
